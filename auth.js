5ab10fc0390c7c8e252e47daade848d6dcd0e3e5
Subham Beura <subhambeura20@gmail.com>
2023-01-20 19:09:35 +0530

    comments

--- auth.js
+++ auth.js
@@ -6,21 +6,30 @@ router.get("/", async (req, res) => {
     res.json(allUsers)
 })
 router.post('/signup', async (req, res) => {
-    console.log(req.body)
     let newUser = req.body
+    // Create Doc
     const doc = new User(newUser)
+    // SaveDoc in DB
     const saved = await doc.save()
     res.send(saved)
 })
 router.post('/login', async (req, res) => {
     const userDetails = req.body
     try {
+        //Find User
         const userFound = await User.findOne({ username: userDetails.username })
+
+        //Wrong UserName
         if (!userFound)
             res.send({ msg: "Wrong UserName" })
+        
+        //Wrong Password e
         if (userDetails.password !== userFound.password)
-            res.json({ msg: "wrong Password", given: userDetails, data: userFound })
+            res.json({ msg: "wrong Password", given: userDetails} )
+        
+        //Ideal
         res.json({ msg: "Logged In", ...userFound, })
+
     } catch {
         err => {
             console.log(err)
